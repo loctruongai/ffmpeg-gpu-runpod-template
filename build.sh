225174736d5b582f5f8618fd601051e3a8a07a9d
Pranjalya <pranjalyatiwari10@gmail.com>
2024-06-03 11:11:58 +0000

    :zap: bump version

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build --platform linux/amd64 --tag pranjalya/ffmpeg-cuda-runpod:latest .
-docker push pranjalya/ffmpeg-cuda-runpod:latest
+docker build --platform linux/amd64 --tag pranjalya/ffmpeg-cuda-runpod:0.4 .
+docker push pranjalya/ffmpeg-cuda-runpod:0.4
